1c0a5ae53873e9851e8c64ad81672cd25681f7fe
costikluciu2020 <costikluciu2020@gmail.com>
2022-08-01 16:16:27 +0100

    Designed a styling look in figma and then implementing it into CSS

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="todo-app">
+		<h1>TODOS</h1>
+		<form>
+			<input type="text"></input>
+			<button>ADD</button>
+		</form>
+		<div className="items-stack">
+			<ul>
+				<li>
+					<div className="star">star</div>
+					<div className="item">Item one</div>
+					<div className="edit">edit</div>
+					<div className="remove">remove</div>
+				</li>
+
+				<li>
+					<div className="star">star</div>
+					<div className="item">Item one</div>
+					<div className="edit">edit</div>
+					<div className="remove">remove</div>
+				</li>
+
+				<li>
+					<div className="star">star</div>
+					<div className="item">Item one</div>
+					<div className="edit">edit</div>
+					<div className="remove">remove</div>
+				</li>
+
+				<li>
+					<div className="star">star</div>
+					<div className="item">Item one</div>
+					<div className="edit">edit</div>
+					<div className="remove">remove</div>
+				</li>
+				
+			</ul>
+		</div>
+
+	</div>
   );
 }
+
+
 
 export default App;
